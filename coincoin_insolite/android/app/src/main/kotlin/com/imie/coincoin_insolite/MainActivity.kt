9d06e4189b7a8ae8ae5f7aabe998767bd1533d78
Bastien <pma.Bastien.aubry@gmail.com>
2019-12-17 11:42:17 +0100

    squelette

---
+++ coincoin_insolite/android/app/src/main/kotlin/com/imie/coincoin_insolite/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.imie.coincoin_insolite
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
